7a65b9d622cf4b0ffdba3d9be9a8ad7fe0ae144c
Ion Gireada <shytiger@yahoo.com>
2023-06-25 11:28:23 +0300

    Small changes

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,8 @@
+/*
+ * Created on Sun Jun 25 2023
+ *
+ * Copyright (c) 2023 ioncakephper
+ */
 const program = require('commander');
 const {
     appLog,
@@ -16,9 +21,9 @@ let { name, version, description } = require("../package.json");
 let {INFO} = {
     "INFO": "INFO"
 }
-program 
+program
     .name(name)
-    .description(description)  
+    .description(description)
     .version(version)
 
     .command("build", { isDefault: true })
@@ -40,7 +45,7 @@ program
                 "__outlines__/**/*.yaml",
                 "__outlines__/**/*.yml",
             ]
-        } 
+        }
         options = {
             ...defaultOptions,
             ...options,
@@ -65,32 +70,31 @@ program
             }
         })
         appLog(INFO, `Outline outline patterns: ${JSON.stringify(files, null, 4)}`, options)
-    
-        let docSidebarDefs = []; 
+
+        let docSidebarDefs = [];
         for (const outlineFilename of files) {
             let localSidebarDefs = loadOutlineSidebars(outlineFilename)
             localSidebarDefs.forEach(item => {
                 docSidebarDefs.push(item)
             })
         }
-    
+
         let allSidebars = {};
         for (const sidebarDef of docSidebarDefs) {
-    
+
             let {label} = sidebarDef;
             allSidebars[label] = buildSidebar(sidebarDef, options)
         }
         appLog(INFO, `Sidebars created: ${JSON.stringify(Object.keys(allSidebars), null, 4)}`, options)
-    
+
         hbsr.options.template_path = options.templates;
         let content = hbsr.render_template('sidebars', {content: `${JSON.stringify(allSidebars, null, 4)}`})
 
         saveDocument(options.sidebar, content)
         appLog(INFO, `Created sidebar file: ${options.sidebar}`, options)
-    
+
     });
 
 // program.parse("node skelo help build".split(" "))
 // program.parse("node skelo skeleton.outline.yml -d ./website/docs -s ./website/sidebars.js".split(" "))
-program.parse() 
- 
+program.parse()
